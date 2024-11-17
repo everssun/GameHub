ba8afb4cdb55defec6cd252554a7e9478ab2b3c1
everssun <xs2421@columbia.edu>
2024-11-17 15:53:16 -0800

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { Button } from "./components/ui/button";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 import { ColorPalette } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette={"blue"}>Button</Button>;
+  const showAside = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024 px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={showAside}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
