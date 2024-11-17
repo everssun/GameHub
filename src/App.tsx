927ad7073e0462efd4cae080f0601a647a3d0649
everssun <xs2421@columbia.edu>
2024-11-17 15:25:08 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./components/ui/button";
+import { ColorPalette } from "@chakra-ui/react";
+
+function App() {
+  return <Button colorPalette={"blue"}>Button</Button>;
+}
+
+export default App;
