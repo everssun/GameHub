1fa37f9b839d1d7964077e172e16d6317728835a
everssun <xs2421@columbia.edu>
2024-11-18 19:26:29 -0800

    Display game icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, Image, Heading } from "@chakra-ui/react";
+import { Card, Image, Heading, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -10,7 +11,10 @@ const GameCard = ({ game }: Props) => {
     <Card.Root borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <Card.Body>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        ></PlatformIconList>
       </Card.Body>
     </Card.Root>
   );
